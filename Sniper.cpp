bf819b833d8762285ce3583d4d009de9644dd5a8
rotemish7 <57870796+rotemish7@users.noreply.github.com>
2020-06-04 14:11:38 +0300

    test sniper

--- Sniper.cpp
+++ Sniper.cpp
@@ -12,25 +12,29 @@ void Sniper::attack(vector<vector<Soldier*>> &b, pair<int,int> location)
     int row=location.first;
     int col=location.second;
     int target=0;
-    pair<pair<int,int>,Soldier*> temp=make_pair(make_pair(0,0),nullptr);
-
-    for(int i= 0; i< b.size(); ++i)
+    double max = 0;
+    Soldier* s;
+    Soldier* enemy;
+    Soldier* me = b[row][col];
+    for(int i = 0; i < b.size(); ++i)
     {
-        for(int j=0; j< b[i].size(); ++j)
+        for(int j = 0; j < b[i].size(); ++j)
         {
-            if(b[i][j]!=nullptr&&b[i][j]->getPlayer_number()!=this->player_number&&b[i][j]->getHp()>target)
+            s = b[i][j];
+            if (s != nullptr && s->getPlayer_number() != me->getPlayer_number())
+                hp = s->getHp();
+            if (hp > max)
             {
-                target=b[i][j]->getHp();
-                temp.first.first=i;
-                temp.first.second=j;
-                temp.second=b[i][j];
+                max = hp;
+                enemy = b[i][j];
             }
         }
     }
-
-    temp.second->setHp(temp.second->getHp()+damage);
-    if(temp.second->getHp()<=0)
+    int damage = me->getDamage();
+    int health = enemy->getHp();
+    enemy->setHp(health+damage);
+    if(!enemy->isAlive())
     {
-        b[temp.first.first][temp.first.second]=nullptr;
+        delete enemy;
     }
 }
