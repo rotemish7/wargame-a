263300568f0a1d9902d1787c6faef5c74b9d7776
rotemish7 <57870796+rotemish7@users.noreply.github.com>
2020-06-04 21:00:03 +0300

    Update SniperCommander.cpp

--- SniperCommander.cpp
+++ SniperCommander.cpp
@@ -1,69 +1,59 @@
-//
-// Created by rotem levy on 27/05/2020.
-//
-
 #include "SniperCommander.hpp"
-#include "Sniper.hpp"
-#include "FootSoldier.hpp"
-#include "Board.hpp"
 
-using namespace std;
-
-void SniperCommander::attack(vector<vector<Soldier*>> &b, pair<int,int> location)
+void SniperCommander::action(std::vector<std::vector<Soldier*>> &board, std::pair<int,int> location)
 {
-    int row = location.first;
-    int col = location.second;
+    int x1 = location.first;
+    int x2 = location.second;
 
-    Soldier* s_enemy = nullptr;
-    Soldier* temp = nullptr;
-    int s_enemy_hp = 0;
-    std::pair<int,int> s_enemy_loc;
-    std::vector<Soldier*> company;
-    std::vector<std::pair<int,int>> company_locs;
+    Soldier* strongest_enemy = nullptr;
+    uint strongest_enemy_hp = 0;
+    std::pair<int,int> strongest_enemy_location;
 
-    for(int i = 0 ; i < b.size() ; i ++)
+    std::vector<Soldier*> teammates;
+    std::vector<std::pair<int,int>> teammates_locations;
+
+    for(int i = 0 ; i < board.size() ; i ++)
     {
-        for(int j = 0 ; j < b[i].size(); j++)
+        for(int j = 0 ; j < board[i].size(); j++)
         {
-            temp = b[i][j];
-            if(temp != nullptr)
+            Soldier* curr = board[i][j];
+            if(curr != nullptr)
             {
-                if(temp->getPlayer_number() != player_number)
+                if(curr->getPlayer_number() != player_number)
                 {
-                    uint temp_hp = temp->getHp();
-                    if(temp_hp > s_enemy_hp)
+                    Soldier* curr = board[i][j];
+                    uint curr_hp = curr->getHp();
+                    if(curr_hp > strongest_enemy_hp)
                     {
-                        s_enemy_hp = temp_hp;
-                        s_enemy = temp;
-                        s_enemy_loc = {i,j};
+                        strongest_enemy_hp = curr_hp;
+                        strongest_enemy = curr;
+                        strongest_enemy_location = {i,j};
                     }
-                }
-                else
-                {
-                    if(temp->getType() == SniperType)
+                } else {
+                    if(curr->getType() == Type::SniperType)
                     {
-                        company.push_back(temp);
-                        company_locs.push_back({i,j});
+                        teammates.push_back(curr);
+                        teammates_locations.push_back({i,j});
                     }
                 }
             }
         }
     }
 
-    if(s_enemy != nullptr)
+    if(strongest_enemy != nullptr)
     {
-        int new_hp = s_enemy->getHp() +damage;
-        s_enemy->setHp(new_hp);
+        int new_hp = strongest_enemy->getHp() +damage;
+        strongest_enemy->setHp(new_hp);
         if(new_hp <= 0)
         {
-            b[s_enemy_loc.first][s_enemy_loc.second] = nullptr;
+            board[strongest_enemy_location.first][strongest_enemy_location.second] = nullptr;
         }
     }
 
-    for(int i = 0 ; i < company.size() ; i++)
+    for(int i = 0 ; i < teammates.size() ; i++)
     {
-        Soldier* teammate = company[i];
-        std::pair<int,int> teammate_loc = company_locs[i];
-        teammate->attack(b,teammate_loc);
+        Soldier* curr_teammate = teammates[i];
+        std::pair<int,int> curr_location = teammates_locations[i];
+        curr_teammate->attack(board,curr_location);
     }
 }
