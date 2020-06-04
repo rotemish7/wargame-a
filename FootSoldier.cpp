905db8ccc454236f8a883c3dc37e1079a336bb88
rotemish7 <57870796+rotemish7@users.noreply.github.com>
2020-06-04 20:30:01 +0300

    Update FootSoldier.cpp

--- FootSoldier.cpp
+++ FootSoldier.cpp
@@ -3,18 +3,8 @@
 //
 
 #include "FootSoldier.hpp"
-#include <cmath>
-#include <iostream>
-#include <vector>
-using namespace std;
 
-double distance(double x1,double y1,double x2,double y2)
-{
-     double dis1 = pow(x1-x2, 2);
-    double dis2 = pow(y1-y2, 2);
-    double ans = sqrt(dis1+dis2);
-    return ans;
-}
+using namespace std;
 
 void FootSoldier::attack(vector<vector<Soldier*>> &b, pair<int,int> location)
 {
