959a578853e32621bea205f34c36fe020957aa1d
rotemish7 <57870796+rotemish7@users.noreply.github.com>
2020-06-04 20:29:27 +0300

    Update FootSoldier.hpp

--- FootSoldier.hpp
+++ FootSoldier.hpp
@@ -2,11 +2,6 @@
 // Created by rotem levy on 27/05/2020.
 //
 
-
-#include <string>
-#include <vector>
-#include <stdexcept>
-#include <cmath>
 #include "Soldier.hpp"
 
 using namespace std;
@@ -16,5 +11,4 @@ class FootSoldier: public Soldier
 public:
     FootSoldier(uint num) : Soldier(num, 100, -10,FootSoldierType) {}
     void attack(vector<vector<Soldier*>> &s, pair<int,int> location);
-    double distance(int x1, int y1, int x2, int y2);
 };
