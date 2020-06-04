fb25f68c19b3de9dc387ca119507f4396e3eec15
rotemish7 <57870796+rotemish7@users.noreply.github.com>
2020-06-04 20:05:49 +0300

    Update FootCommander.cpp

--- FootCommander.cpp
+++ FootCommander.cpp
@@ -28,7 +28,7 @@ void FootCommander::attack(vector<vector<Soldier*>> &b, pair<int,int> location)
             {
                 if(temp->getPlayer_number() != b[row][col]->getPlayer_number())
                 {
-                    dist = distance(row,col,i,j);
+                    dist = FootSoldier::distance(row,col,i,j);
                     if(dist<min)
                     {
                         min = dist;
