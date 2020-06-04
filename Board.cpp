48685217edd291144764b9cc9d984469157083bb
rotemish7 <57870796+rotemish7@users.noreply.github.com>
2020-06-04 19:58:02 +0300

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -9,7 +9,7 @@ using namespace WarGame;
 
     double Utils::distance(double x1,double y1,double x2,double y2)
     {
-    return sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+        return sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
     }
    
     Soldier*& Board::operator[](std::pair<int, int> location)
