2b0b2d679019a7228959118254c073e6309c63cb
rotemish7 <57870796+rotemish7@users.noreply.github.com>
2020-06-04 19:17:33 +0300

    Update FootSoldier.hpp

--- FootSoldier.hpp
+++ FootSoldier.hpp
@@ -14,4 +14,5 @@ class FootSoldier: public Soldier
 public:
     FootSoldier(uint num) : Soldier(num, 100, -10,FootSoldierType) {}
     void attack(vector<vector<Soldier*>> &s, pair<int,int> location);
+    double distance(int x1, int y1, int x2, int y2);
 };
