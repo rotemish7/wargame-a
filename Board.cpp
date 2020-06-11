3fad90aaebd128b42f7f9e569b33acc7dc048575
rotemish7 <57870796+rotemish7@users.noreply.github.com>
2020-06-11 14:05:53 +0300

    Add files via upload

--- Board.cpp
+++ Board.cpp
@@ -3,90 +3,92 @@
 //
 
 #include "Board.hpp"
-#include <iostream>
-
 using namespace WarGame;
 
-    double Utils::distance(double x1,double y1,double x2,double y2)
+double Utils::distance(double x1,double y1,double x2,double y2)
+{
+    return sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+}
+
+Soldier*& Board::operator[](std::pair<int,int> location)
+{
+    return board[location.first][location.second];
+}
+
+Soldier* Board::operator[](std::pair<int,int> location) const
+{
+    return board[location.first][location.second];
+}
+
+void Board::move(uint player_number, std::pair<int,int> source, MoveDIR direction)
+{
+    if(source.first < 0 || source.second < 0 || source.first >= board.size() || source.second >= board.size())
     {
-        return sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+        throw std::invalid_argument("Invalid source location!");
     }
 
-    Soldier*& Board::operator[](std::pair<int, int> location)
+    Soldier* soldier_chose = board[source.first][source.second];
+    if(soldier_chose == nullptr)
     {
-        return board[location.first][location.second];
+        throw std::invalid_argument("There is no Soldier on source location!");
     }
 
-    Soldier* Board::operator[](std::pair<int, int> location) const
+    if(soldier_chose->getPlayerNumber() != player_number)
     {
-        return board[location.first][location.second];
+        throw std::invalid_argument("This is not your Soldier!");
     }
 
-    void Board::move(uint player_number, std::pair<int, int> source, WarGame::Board::MoveDIR direction)
+    std::pair<int,int> target = source;
+    switch (direction)
     {
-        Soldier* soldier = (*this)[source];
+        case MoveDIR::Up:
+            target.first = source.first+1;
+            break;
+        case MoveDIR::Down:
+            target.first = source.first-1;
+            break;
+        case MoveDIR::Left:
+            target.second = source.second-1;
+            break;
+        case MoveDIR::Right:
+            target.second = source.second+1;
+            break;
 
-        if(soldier==nullptr||soldier->getPlayer_number()!=player_number)
-        {
-            throw std::invalid_argument("No soldier available");
-        }
-
-        std::pair<int, int> move;
-        switch(direction)
-        {
-            case Up:
-            {
-                move=std::make_pair(source.first+1,source.second);
-                break;
-            }
-            case Down:
-            {
-                move=std::make_pair(source.first-1,source.second);
-                break;
-            }
-            case Left:
-            {
-                move=std::make_pair(source.first,source.second-1);
-                break;
-            }
-            case Right:
-            {
-                move=std::make_pair(source.first,source.second+1);
-                break;
-            }
-            default:
-                break;
-        }
-
-        if(move.first>board.size()-1||move.second>board.size()-1||move.first<0||move.second<0)
-        {
-            throw std::invalid_argument("Exited board limits ");
-        }
-
-        if((*this)[move] != nullptr)
-        {
-            throw std::runtime_error("place already taken by another player\n");
-        }
-
-        board[source.first][source.second]=nullptr;
-        board[move.first][move.second]=soldier;
-        soldier->attack(board,move);
+        default:
+            break;
     }
 
-    bool Board::has_soldiers(uint player_number) const
+    if(target.first < 0 || target.second < 0 || target.first >= board.size() || target.second >= board[target.first].size())
     {
-        for (int i=0;i<board.size();i++)
+        throw std::invalid_argument("Invalid Move!");
+    }
+
+    if(board[target.first][target.second] != nullptr)
+    {
+        throw std::invalid_argument("There is already a Soldier in target location!");
+    }
+
+    board[target.first][target.second] = soldier_chose;
+    board[source.first][source.second] = nullptr;
+
+    soldier_chose->action(board,target);
+
+}
+
+bool Board::has_soldiers(uint player_number) const
+{
+    for(int i = 0; i<board.size(); i++)
+    {
+        for(int j = 0 ; j < board[i].size() ; j++)
         {
-            for(int j=0;i<board[0].size();j++)
+            if(board[i][j] != nullptr)
             {
-                if(board[i][j] != nullptr)
+                if(board[i][j]->getPlayerNumber() == player_number)
                 {
-                    if(board[i][j]->getPlayer_number()==player_number)
-                    {
-                        return true;
-                    }
+                    return true;
                 }
             }
         }
-        return false;
     }
+    return false;
+}
