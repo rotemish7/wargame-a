a9f4db2b7939e5b4c5e1fe3b3e4d7a0c9fc21db2
rotemish7 <57870796+rotemish7@users.noreply.github.com>
2020-06-11 13:49:52 +0300

    Update ParamedicCommander.cpp

--- ParamedicCommander.cpp
+++ ParamedicCommander.cpp
@@ -24,11 +24,11 @@ void ParamedicCommander::attack(std::vector<std::vector<Soldier*>> &b, std::pair
     int row = location.first;
     int col = location.second;
 
-    for(int i = x - 1; i <= x+1 && i <b.size(); i++)
+    for(int i = row - 1; i <= row+1 && i <b.size(); i++)
     {
         if(i < 0) continue;
         int size = b[i].size();
-        for(int j = (y - 1); j < size && j <= (y+1); j++)
+        for(int j = (col - 1); j < size && j <= (col+1); j++)
         {
             if(j < 0) continue;
 
